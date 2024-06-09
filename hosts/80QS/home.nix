6a81135ae40db60ceab510c930f3302d5ea2e0ab
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2024-06-09 09:29:32 +0800

    feat: add tmux

--- hosts/80QS/home.nix
+++ hosts/80QS/home.nix
@@ -24,6 +24,7 @@
     ../../modules/home-manager/wezterm
     ../../modules/home-manager/screenshot
     ../../modules/home-manager/vscode.nix
+    ../../modules/home-manager/tmux.nix
   ];
 
   nixpkgs = {
