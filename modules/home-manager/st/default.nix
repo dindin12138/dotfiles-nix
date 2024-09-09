af1e8690a26ef4f24e862ceb7344cd711d4e0a13
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2024-09-09 22:09:30 +0800

    feat: add st desktopentry & config keymap

--- modules/home-manager/st/default.nix
+++ modules/home-manager/st/default.nix
@@ -4,8 +4,10 @@
     (st.overrideAttrs (oldAttrs: rec {
       configFile = writeText "config.def.h" (builtins.readFile ./config.h);
       postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+      buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
       patches = [
         ./patches/st-scrollback-ringbuffer-0.9.2.diff
+        ./patches/st-ligatures-scrollback-ringbuffer-20240427-0.9.2.diff
       ];
     }))
   ];
