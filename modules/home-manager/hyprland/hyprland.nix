4c829eb8ea309e1b94e1226f57f6565743fd8a53
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2024-09-16 13:22:07 +0800

    feat: update steam windowsrule

--- modules/home-manager/hyprland/hyprland.nix
+++ modules/home-manager/hyprland/hyprland.nix
@@ -54,6 +54,7 @@
         # Steam
         "float, class:(steam), title:(Friends List|Special Offers|Steam Settings|好友列表|特惠|Steam 设置)"
         "center, class:(steam), title:(关机)"
+        "size 20% 80%, class:(steam), title:(好友列表)"
         # Zotero
         "float, class:^(Zotero)$, title:^(Zotero Settings|Add-ons Manager|Plugins Manager|Add-on Market|Progress|Import|高级搜索|Advanced Search|Software Update)$"
         "center, class:^(Zotero)$, title:^(Zotero Settings|Add-ons Manager|Plugins Manager|Add-on Market|高级搜索|Advanced Search|Software Update)$"
