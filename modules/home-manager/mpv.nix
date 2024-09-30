582e39351bdd897997b59730be195ad7427ca671
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2024-09-30 21:19:23 +0100

    feat: update mpv config

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -1,9 +1,14 @@
+{ config, ... }:
 {
   programs.mpv = {
     enable = true;
     bindings = { };
     config = {
       save-position-on-quit = true;
+      save-file-auto = "fuzzy";
+      sub-auto = "fuzzy";
+      keep-open = true;
+      screenshot-directory = "${config.home.homeDirectory}/Pictures/Screenshots";
     };
   };
 }
