33ed563a0cdd83fafc6cbbb712ce9cb2952324d5
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2024-10-11 21:33:58 +0100

    feat: disable gc

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -10,11 +10,11 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
       ];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 7d";
+    # };
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
     registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
