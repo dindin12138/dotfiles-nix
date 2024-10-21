56868aa4a843567f4cc44bae6de1be92074dae50
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2024-10-21 23:33:35 +0100

    feat: update sddm theme config

--- modules/nixos/display-manager/default.nix
+++ modules/nixos/display-manager/default.nix
@@ -3,7 +3,8 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "${import ./catppuccin.nix { inherit pkgs; }}";
+    package = pkgs.kdePackages.sddm;
+    theme = "catppuccin-mocha";
     settings = {
       Theme = {
         CursorTheme = "Bibata-Modern-Ice";
@@ -13,5 +14,14 @@
   };
   environment.systemPackages = with pkgs; [
     bibata-cursors
+    (
+      pkgs.catppuccin-sddm.override {
+        flavor = "mocha";
+        font = "FiraCode Nerd Font";
+        fontSize = "16";
+        background = "${../../home-manager/wall/wallpapers/background.png}";
+        loginBackground = false;
+      }
+    )
   ];
 }
