8864e7c097a1a7f74c28c602c30242994c460a91
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-05-14 01:01:40 +0100

    feat: comment useless substituters

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -5,7 +5,7 @@
       experimental-features = "nix-command flakes";
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
-      substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store" ];
+      # substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store" ];
     };
     # gc = {
     #   automatic = true;
