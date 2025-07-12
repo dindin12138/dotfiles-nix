715efc26640a7d349871d18d5ee8c8da2837e850
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-07-12 22:59:04 +0100

    feat: update quickshell & qt6

--- modules/home-manager/quickshell.nix
+++ modules/home-manager/quickshell.nix
@@ -1,6 +1,28 @@
-{
+{ pkgs, ... }: {
+
   programs.quickshell = {
     enable = true;
     activeConfig = null;
   };
+  home.packages = with pkgs; [
+    # kdePackages.qt6ct
+    kdePackages.qt5compat
+    kdePackages.qtbase
+    kdePackages.qtdeclarative
+    kdePackages.qtimageformats
+    kdePackages.qtmultimedia
+    kdePackages.qtpositioning
+    kdePackages.qtquicktimeline
+    kdePackages.qtsensors
+    kdePackages.qtsvg
+    kdePackages.qttools
+    kdePackages.qttranslations
+    kdePackages.qtvirtualkeyboard
+    kdePackages.qtwayland
+    kdePackages.qt5compat
+    kdePackages.syntax-highlighting
+
+    libsForQt5.kdialog
+    libsForQt5.qt5.qtgraphicaleffects
+  ];
 }
