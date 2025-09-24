ea8d66ce61d5cf6089a87cd08394159dd5a9462e
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-09-24 10:21:12 +0800

    feat: enable auto gc

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -13,11 +13,11 @@
       auto-optimise-store = true;
       substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store" ];
     };
-    # gc = {
-    #   automatic = true;
-    #   dates = "weekly";
-    #   options = "--delete-older-than 7d";
-    # };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
     registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
