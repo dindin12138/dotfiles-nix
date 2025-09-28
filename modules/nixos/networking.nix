7d3d3d43f192305e708389004a948acf177756a2
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-09-28 16:40:53 +0800

    feat: switch proxy IP

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -2,6 +2,7 @@
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
-    proxy.default = "http://127.0.0.1:7897";
+    # proxy.default = "http://127.0.0.1:7897";
+    proxy.default = "http://192.168.0.105:7897";
   };
 }
--- modules/nixos/proxy.nix
+++ modules/nixos/proxy.nix
@@ -1,6 +1,8 @@
 {
   systemd.services.nix-daemon.environment = {
-    http_proxy = "http://127.0.0.1:7897";
-    https_proxy = "http://127.0.0.1:7897";
+    # http_proxy = "http://127.0.0.1:7897";
+    # https_proxy = "http://127.0.0.1:7897";
+    http_proxy = "http://192.168.0.105:7897";
+    https_proxy = "http://192.168.0.105:7897";
   };
 }
