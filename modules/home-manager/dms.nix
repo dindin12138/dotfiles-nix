581a41e45214158940a3a4a9c28bee90293754f7
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-10-21 18:47:08 +0800

    feat: update

--- modules/home-manager/dms.nix
+++ modules/home-manager/dms.nix
@@ -3,9 +3,114 @@
     enable = true;
     enableSystemd = true;
     enableDynamicTheming = false;
-    # default.settings = {
-    #   "theme.use24HourClock" = true;
-    #   "theme.showSeconds" = true;
-    # };
+    default.settings = {
+      "wallpaperFillMode" = "Fill";
+
+      "use24HourClock" = true;
+
+      "clockDateFormat" = "ddd MMM d";
+      "lockDateFormat" = "ddd MMM d";
+
+      "weatherEnabled" = true;
+      "useAutoLocation" = true;
+      "useFahrenheit" = false;
+
+      # Dank Bar
+      "dankBarPosition" = 0;
+      "dankBarAutoHide" = false;
+      "dankBarOpenOnOverview" = true;
+
+      "dankBarTransparency" = 1;
+      "dankBarWidgetTransparency" = 1;
+
+      "dankBarSpacing" = 0;
+      "dankBarBottomGap" = 0;
+      "dankBarInnerPadding" = 8;
+
+      "dankBarSquareCorners" = true;
+      "dankBarNoBackground" = false;
+      "dankBarGothCornersEnabled" = false;
+      "dankBarBorderEnabled" = false;
+
+      "dankBarFontScale" = 1.1;
+
+      "dankBarLeftWidgets" = [
+        "launcherButton"
+        "workspaceSwitcher"
+        "focusedWindow"
+      ];
+
+      "dankBarCenterWidgets" = [
+        {
+          "id" = "music";
+          "enabled" = true;
+        }
+        "clock"
+        "weather"
+      ];
+
+      "dankBarRightWidgets" = [
+        {
+          "id" = "systemTray";
+          "enabled" = true;
+        }
+        "clipboard"
+        {
+          "id" = "cpuUsage";
+          "enabled" = true;
+          "minimumWidth" = false;
+        }
+        {
+          "id" = "memUsage";
+          "enabled" = true;
+          "minimumWidth" = false;
+        }
+        "notificationButton"
+        "battery"
+        {
+          "id" = "controlCenterButton";
+          "enabled" = true;
+          "showNetworkIcon" = true;
+          "showBluetoothIcon" = true;
+          "showAudioIcon" = true;
+        }
+        {
+          "id" = "idleInhibitor";
+          "enabled" = true;
+        }
+      ];
+
+      "showWorkspaceIndex" = false;
+      "workspaceScrolling" = false;
+      "showWorkspacePadding" = false;
+      "showWorkspaceApps" = false;
+      "workspacesPerMonitor" = true;
+
+      "notificationPopupPosition" = 0;
+
+      "cornerRadius" = 15;
+
+      "fontFamily" = "FiraCode Nerd Font";
+      "monoFontFamily" = "FiraCode Nerd Font Mono";
+      "fontWeight" = 400;
+      "fontScale" = 1.1;
+
+      "syncModeWithPortal" = true;
+      "iconTheme" = "System Default";
+
+      "lockScreenShowPowerActions" = true;
+      "loginctlLockIntegration" = true;
+      "lockBeforeSuspend" = true;
+
+      "launcherLogoMode" = "os";
+
+      # Popup
+      "popupTransparency" = 1;
+      "popupGapsAuto" = false;
+      "popupGapsManual" = 7;
+
+      # Dock
+      "showDock" = false;
+    };
   };
 }
