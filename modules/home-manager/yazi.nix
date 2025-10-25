5f9347a09f645c75077ac85d543e57d737fcf8cc
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-10-26 00:01:26 +0800

    feat: move yazi

---
+++ modules/home-manager/yazi.nix
@@ -0,0 +1,108 @@
+{ pkgs, ... }:
+{
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    extraPackages = with pkgs; [ ];
+    keymap = { };
+    settings = {
+      mgr = {
+        ratio = [
+          1
+          2
+          4
+        ];
+        sort_by = "natural";
+      };
+      preview = {
+        max_width = 1800;
+        max_height = 1800;
+      };
+    };
+    theme = {
+      mgr = {
+        preview_hovered = {
+          reversed = true;
+        };
+        border_symbol = " ";
+      };
+      icon = {
+        dirs = [
+          {
+            name = "Desktop";
+            text = "";
+          }
+          {
+            name = "Development";
+            text = "";
+          }
+          {
+            name = "Documents";
+            text = "";
+          }
+          {
+            name = "Downloads";
+            text = "";
+          }
+          {
+            name = "Library";
+            text = "";
+          }
+          {
+            name = "Movies";
+            text = "";
+          }
+          {
+            name = "Music";
+            text = "";
+          }
+          {
+            name = "Pictures";
+            text = "";
+          }
+          {
+            name = "Public";
+            text = "";
+          }
+          {
+            name = "Videos";
+            text = "";
+          }
+          {
+            name = "Workspace";
+            text = "";
+          }
+          {
+            name = "Zotero";
+            text = "󰰶";
+          }
+          {
+            name = "Templates";
+            text = "";
+          }
+          {
+            name = "Screenshots";
+            text = "󰸉";
+          }
+          {
+            name = "Wallpapers";
+            text = "󰸉";
+          }
+          {
+            name = "Larian Studios";
+            text = "󱎓";
+          }
+          {
+            name = "VM";
+            text = "";
+          }
+          {
+            name = "ISO";
+            text = "";
+          }
+        ];
+      };
+    };
+  };
+}
