2bd6b3aa6e2999914cf06620da6b4ca1df45cd8c
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-10-26 12:55:08 +0800

    feat: enable local proxy

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -13,7 +13,6 @@
         };
       };
     };
-    # proxy.default = "http://127.0.0.1:7897";
-    proxy.default = "http://192.168.0.105:7897";
+    proxy.default = "http://127.0.0.1:7897";
   };
 }
