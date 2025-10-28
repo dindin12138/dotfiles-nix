d38e9ee81141840a212c0a99283d6118624bbb3b
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-10-28 11:59:05 +0800

    feat: update zed

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -1,9 +1,31 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   stylix.targets.zed.enable = true;
   programs.zed-editor = {
     enable = true;
-    extensions = [ ];
-    extraPackages = with pkgs; [ ];
+    extensions = [
+      "material-icon-theme"
+      "nix"
+    ];
+    extraPackages = with pkgs; [
+      nixd
+      nil
+      clang-tools
+    ];
+    mutableUserSettings = true;
+    userSettings = {
+      ui_font_size = lib.mkForce (config.stylix.fonts.sizes.applications + 6);
+      buffer_font_size = lib.mkForce (config.stylix.fonts.sizes.terminal + 2);
+      agent_servers = {
+        gemini = {
+          ignore_system_version = false;
+        };
+      };
+    };
   };
 }
