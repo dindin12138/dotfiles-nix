e9cc2bcd53f550d0e6ed8b9bb59dc15f15b4ae9c
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-11-27 19:01:51 +0800

    feat: update zed font size

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -4,6 +4,10 @@
   lib,
   ...
 }:
+let
+  isLinux = pkgs.stdenv.isLinux;
+  isDarwin = pkgs.stdenv.isDarwin;
+in
 {
   stylix.targets.zed.enable = true;
   programs.zed-editor = {
@@ -23,8 +27,8 @@
     ];
     mutableUserSettings = true;
     userSettings = {
-      ui_font_size = lib.mkForce (config.stylix.fonts.sizes.applications + 6);
-      buffer_font_size = lib.mkForce (config.stylix.fonts.sizes.terminal + 2);
+      ui_font_size = lib.mkForce (config.stylix.fonts.sizes.applications + (if isLinux then 6 else 0));
+      buffer_font_size = lib.mkForce (config.stylix.fonts.sizes.terminal + (if isLinux then 2 else 0));
       agent_servers = {
         gemini = {
           ignore_system_version = false;
