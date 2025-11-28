ecdd136fed4e1c2431f7f9d0bd99fc289bea8cf8
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-11-28 21:12:18 +0800

    feat: update greeter & switch to greetd

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -1,7 +1,38 @@
+{ pkgs, config, ... }:
 {
   programs.dankMaterialShell.greeter = {
     enable = true;
-    compositor.name = "niri";
     configHome = "/home/din";
+    compositor = {
+      name = "niri"; # Required. Can be also "hyprland" or "sway"
+      customConfig = ''
+        output "eDP-1" {
+            scale 1.5
+        }
+        hotkey-overlay {
+            skip-at-startup
+        }
+        environment {
+            DMS_RUN_GREETER "1"
+        }
+        cursor {
+            xcursor-theme "${config.stylix.cursor.name}"
+            xcursor-size ${toString config.stylix.cursor.size}
+        }
+        gestures {
+            hot-corners {
+                off
+            }
+        }
+      '';
+    };
+    logs = {
+      save = true;
+      path = "/tmp/dms-greeter.log";
+    };
   };
+  users.users.din.extraGroups = [ "greeter" ];
+  environment.systemPackages = with pkgs; [
+    bibata-cursors
+  ];
 }
