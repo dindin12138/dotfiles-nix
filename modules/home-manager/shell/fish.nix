bfe754a26719e6d588ac401aa2ba9db6f83fce25
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-12-04 22:05:29 +0800

    feat: update proxy config

--- modules/home-manager/shell/fish.nix
+++ modules/home-manager/shell/fish.nix
@@ -60,11 +60,13 @@
       proxy-on = ''
         set -gx http_proxy "http://127.0.0.1:7897"
         set -gx https_proxy "http://127.0.0.1:7897"
+        set -gx all_proxy "http://127.0.0.1:7897"
         echo "✅ HTTP Proxy has been set for Fish shell."
       '';
       proxy-off = ''
         set -e http_proxy
         set -e https_proxy
+        set -e all_proxy
         echo "❌ HTTP Proxy has been unset for Fish shell."
       '';
       nix-fzf-search = ''
--- modules/nixos/proxy.nix
+++ modules/nixos/proxy.nix
@@ -1,4 +1,5 @@
 {
+  networking.proxy.default = "http://127.0.0.1:7897";
   systemd.services.nix-daemon.environment = {
     http_proxy = "http://127.0.0.1:7897";
     https_proxy = "http://127.0.0.1:7897";
