1ae6912c457d16311af2dfbe86cb571d7ee7b91a
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-12-05 00:56:27 +0800

    feat: switch to rime-ice

--- modules/home-manager/fcitx5.nix
+++ modules/home-manager/fcitx5.nix
@@ -6,8 +6,11 @@
     type = "fcitx5";
     fcitx5 = {
       addons = with pkgs; [
-        fcitx5-rime
-        qt6Packages.fcitx5-chinese-addons
+        (fcitx5-rime.override {
+          rimeDataPkgs = [
+            pkgs.rime-ice
+          ];
+        })
       ];
       settings = {
         inputMethod = {
@@ -29,4 +32,11 @@
       };
     };
   };
+  xdg.dataFile."fcitx5/rime/default.custom.yaml".text = ''
+    patch:
+      __include: rime_ice_suggestion:/
+
+      schema_list:
+        - schema: rime_ice
+  '';
 }
