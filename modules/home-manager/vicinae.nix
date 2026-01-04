a99637638c1e0e4efc264f67cd606a5d5cae4f07
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-01-04 23:36:27 +0800

    feat: enable noctalia-shell & update

--- modules/home-manager/vicinae.nix
+++ modules/home-manager/vicinae.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  # stylix.targets.vicinae.enable = true;
+  stylix.targets.vicinae.enable = true;
   programs.vicinae = {
     enable = true;
     systemd = {
@@ -9,19 +9,19 @@
     };
     extensions = [ ];
     # useLayerShell = false;
-    settings = {
-      # launcher_window.layer_shell.enabled = false;
-      font = {
-        normal = config.stylix.fonts.monospace.name;
-        size = config.stylix.fonts.sizes.applications - 1;
-      };
-      popToRootOnClose = true;
-      rootSearch = {
-        searchFiles = false;
-      };
-      window = {
-        csd = true;
-      };
-    };
+    # settings = {
+    #   # launcher_window.layer_shell.enabled = false;
+    #   font = {
+    #     normal = config.stylix.fonts.monospace.name;
+    #     size = config.stylix.fonts.sizes.applications - 1;
+    #   };
+    #   popToRootOnClose = true;
+    #   rootSearch = {
+    #     searchFiles = false;
+    #   };
+    #   window = {
+    #     csd = true;
+    #   };
+    # };
   };
 }
