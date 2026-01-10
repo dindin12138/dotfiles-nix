d9527d9b35c40f35fa73cb937ec5d59f6c136845
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-01-10 15:21:59 +0800

    feat(devenv): add cargo proxy rsproxy

--- modules/home-manager/env.nix
+++ modules/home-manager/env.nix
@@ -8,4 +8,14 @@
   home.packages = with pkgs; [
     devenv
   ];
+  home.file.".cargo/config.toml".text = ''
+    [source.crates-io]
+    replace-with = 'rsproxy-sparse'
+
+    [source.rsproxy-sparse]
+    registry = "sparse+https://rsproxy.cn/index/"
+
+    [net]
+    git-fetch-with-cli = true
+  '';
 }
