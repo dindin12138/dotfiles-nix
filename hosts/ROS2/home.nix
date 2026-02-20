b13a0739fcbd6a39c834db3fa740dd0f30f36d20
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-02-20 18:05:00 +0800

    feat(ghostty) add ghostty for ROS2

--- hosts/ROS2/home.nix
+++ hosts/ROS2/home.nix
@@ -28,6 +28,24 @@
     _7zz
   ];
 
+  stylix.targets.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    package = null;
+    systemd.enable = false;
+    enableFishIntegration = true;
+    settings = {
+      "macos-titlebar-style" = "transparent";
+      "macos-option-as-alt" = true;
+      "window-padding-x" = 10;
+      "window-padding-y" = "10, 0";
+      "mouse-hide-while-typing" = true;
+      "copy-on-select" = true;
+      "cursor-style" = "bar";
+      "cursor-style-blink" = true;
+    };
+  };
+
   nixpkgs = {
     config = {
       allowUnfree = true;
